7fd7ca208dddedc6c60edccf103c70509feed04e
OzodbekXolmurodov <ozodbekxolmurodov08@gmail.com>
2025-06-16 15:34:00 +0500

    uy ishi

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Header from "../header/header";
+import Header from "../Header/header";
 import "./style.css";
 
 function Home() {
